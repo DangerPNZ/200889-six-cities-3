a41ab539a8a4fce5b3328228e1d0302aa730a4fd
DangerPNZ <dangerpnz@mail.ru>
2020-02-25 05:16:24 +0300

    Табы из списка

--- src/components/cities-tabs/cities-tabs.jsx
+++ src/components/cities-tabs/cities-tabs.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {City} from '../../utils/utils.js';
+import nanoid from 'nanoid';
 
 const setCSSTabClasses = (activeCity, tabCity) => activeCity === tabCity ? `locations__item-link tabs__item tabs__item--active` : `locations__item-link tabs__item`;
+/*
+  Допускаю, что тебе данная реализация может не понравиться.
+  Как альтернативу можно конечно использовать связывание двух объектов через Map
+  (города и предложения - два разных массива), но мне больше этот способ понравился.
+  Недостаток один - приходится применять лишнюю проверку. (if внури for, линтер заставляет).
+  Хотя, если ты одобришь вариант для теста, который я предложил, if как раз тут будет нужен, только с другим условием
+*/
+const getCitiesList = (cities) => {
+  const citiesList = [];
+  for (let cityName in cities) {
+    if (City[cityName]) {
+      citiesList.push(cities[cityName]);
+    }
+  }
+  return citiesList;
+};
 
 export class CitiesTabs extends React.PureComponent {
   render() {
@@ -10,36 +27,18 @@ export class CitiesTabs extends React.PureComponent {
       <div className="tabs">
         <section className="locations container">
           <ul className="locations__list tabs__list">
-            <li className="locations__item">
-              <a className={setCSSTabClasses(this.props.selectedCity, City.PARIS)} onClick={() => this.props.onCityTabClick(City.PARIS)} href="#">
-                <span>Paris</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className={setCSSTabClasses(this.props.selectedCity, City.COLOGNE)} onClick={() => this.props.onCityTabClick(City.COLOGNE)} href="#">
-                <span>Cologne</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className={setCSSTabClasses(this.props.selectedCity, City.BRUSSELS)} onClick={() => this.props.onCityTabClick(City.BRUSSELS)} href="#">
-                <span>Brussels</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className={setCSSTabClasses(this.props.selectedCity, City.AMSTERDAM)} onClick={() => this.props.onCityTabClick(City.AMSTERDAM)} href="#">
-                <span>Amsterdam</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className={setCSSTabClasses(this.props.selectedCity, City.HAMBURG)} onClick={() => this.props.onCityTabClick(City.HAMBURG)} href="#">
-                <span>Hamburg</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className={setCSSTabClasses(this.props.selectedCity, City.DUSSELDORF)} onClick={() => this.props.onCityTabClick(City.DUSSELDORF)} href="#">
-                <span>Dusseldorf</span>
-              </a>
-            </li>
+            {
+              getCitiesList(City).map((city) => (
+                /* Создавать тут еще один компонент считаю избыточным */
+                <React.Fragment key={nanoid()}>
+                  <li className="locations__item">
+                    <a className={setCSSTabClasses(this.props.selectedCity, city)} onClick={() => this.props.onCityTabClick(city)} href="#">
+                      <span>{city}</span>
+                    </a>
+                  </li>
+                </React.Fragment>)
+              )
+            }
           </ul>
         </section>
       </div>
